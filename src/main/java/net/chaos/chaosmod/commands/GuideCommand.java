a6f42fac9460c407f0395371d5ae6deaa21d2254
adel.belkercha <adel.belkercha@epita.fr>
2025-03-12 02:11:55 +0100

    feat: after finally opening the GUI for the guide, we're able to draw something on it

--- src/main/java/net/chaos/chaosmod/commands/GuideCommand.java
+++ src/main/java/net/chaos/chaosmod/commands/GuideCommand.java
@@ -1,13 +1,13 @@
 package net.chaos.chaosmod.commands;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
+import javax.annotation.Nullable;
 
 import net.chaos.chaosmod.Main;
+import net.chaos.chaosmod.init.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.WorldClient;
@@ -15,19 +15,40 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraft.world.World;
 import util.Reference;
 import util.guide.GenerateGuideContent;
 import util.guide.model.block.GuideBlock;
 
 public class GuideCommand extends CommandBase {
+	public List<String> aliases = new ArrayList<String>();
+	public List<String> completion = new ArrayList<String>();
 
 	@Override
 	public String getName() {
 		return "guide";
 	}
+	
+	@Override
+    public List<String> getAliases()
+    {
+        aliases.add("gui");
+        return aliases;
+    }
+
+	@Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
+    {
+		if (completion.isEmpty()) {
+			for (Item i : ModItems.ITEMS) {
+				this.completion.add(i.getRegistryName().toString());
+			}
+		}
+		return this.completion;
+    }
 
 	@Override
 	public String getUsage(ICommandSender sender) {
@@ -42,14 +63,12 @@ public class GuideCommand extends CommandBase {
 		String item_id = null;
 		if (args.length == 1) {
 			item_id = args[0];
-			System.out.println("ITEM_ID before path processing = " + item_id);
 		} else {
 			item_id = "chaosmod:oxonium_ore";
 		}
 		System.out.println("substring : " + item_id.substring(0, 9));
 		if (item_id.substring(0, 9).equals(Reference.MODID + ":")) {
 			item_id = item_id.substring(9);
-			System.out.println("ITEM_ID after command = " + item_id);
 		}
 		GenerateGuideContent util = new GenerateGuideContent();
 		String content = null;
@@ -58,7 +77,6 @@ public class GuideCommand extends CommandBase {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.err.println("The item_id : " + item_id);
 		GuideBlock ore_guide = null;
 		try {
 			ore_guide = util.getGuideBlock(content);
