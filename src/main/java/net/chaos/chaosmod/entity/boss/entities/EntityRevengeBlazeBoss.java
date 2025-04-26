a22d624faa235207212e22a5f3883700ab6181cf
adel.belkercha <adel.belkercha@epita.fr>
2025-04-26 12:38:11 +0200

    fix: creativetab the legacy display without sort is better I'll leave it like this 100 commits YAY!

---
+++ src/main/java/net/chaos/chaosmod/entity/boss/entities/EntityRevengeBlazeBoss.java
@@ -0,0 +1,12 @@
+package net.chaos.chaosmod.entity.boss.entities;
+
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.world.World;
+
+public class EntityRevengeBlazeBoss extends EntityBlaze {
+
+	public EntityRevengeBlazeBoss(World worldIn) {
+		super(worldIn);
+	}
+
+}
--- src/main/java/net/chaos/chaosmod/tabs/GeneralTab.java
+++ src/main/java/net/chaos/chaosmod/tabs/GeneralTab.java
@@ -1,9 +1,14 @@
 package net.chaos.chaosmod.tabs;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
+import net.chaos.chaosmod.init.ModBlocks;
 import net.chaos.chaosmod.init.ModItems;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
@@ -23,7 +28,53 @@ public class GeneralTab extends CreativeTabs {
 	public void displayAllRelevantItems(NonNullList<ItemStack> list) {
 		super.displayAllRelevantItems(list);
 		
-		list.sort(Comparator.comparing(ItemStack::getDisplayName));
+		/*
+		 * Sorting by name
+		 */
+		// list.sort(Comparator.comparing(ItemStack::getDisplayName)); 
+
+		/*
+		 * Sorting by type
+		 */
+		/*list.clear();
+		// OXONIUM
+		list.add(new ItemStack(ModBlocks.OXONIUM_ORE));
+		list.add(new ItemStack(ModItems.OXONIUM));
+		list.add(new ItemStack(ModItems.OXONIUM_SWORD));
+		list.add(new ItemStack(ModItems.OXONIUM_PICKAXE));
+		list.add(new ItemStack(ModItems.OXONIUM_AXE));
+		list.add(new ItemStack(ModItems.OXONIUM_SHOVEL));
+		list.add(new ItemStack(ModItems.OXONIUM_HOE));
+		list.add(new ItemStack(ModItems.OXONIUM_HELMET));
+		list.add(new ItemStack(ModItems.OXONIUM_CHESTPLATE));
+		list.add(new ItemStack(ModItems.OXONIUM_LEGGINGS));
+		list.add(new ItemStack(ModItems.OXONIUM_BOOTS));
+		list.add(new ItemStack(ModItems.OXONIUM_CARROT));
+		list.add(new ItemStack(ModBlocks.OXONIUM_BLOCK));
+		list.add(new ItemStack(ModBlocks.OXONIUM_STAIRS));
+		list.add(new ItemStack(ModBlocks.OXONIUM_BRICKS));
+		list.add(new ItemStack(ModBlocks.OXONIUM_CHEST));
+		list.add(new ItemStack(ModItems.OXONIUM_UPGRADE));
+		list.add(new ItemStack(ModBlocks.OXONIUM_FURNACE));
+		
+		// ALLEMANITE
+		list.add(new ItemStack(ModBlocks.ALLEMANITE_ORE));
+		list.add(new ItemStack(ModItems.ALLEMANITE_INGOT));
+		list.add(new ItemStack(ModItems.ALLEMANITE_EXTINGUISHER));
+		list.add(new ItemStack(ModItems.ALLEMANITE_BACKPACK));
+		list.add(new ItemStack(ModBlocks.ALLEMANITE_BLOCK));
+		list.add(new ItemStack(ModBlocks.ALLEMANITE_BRICKS));
+		
+		// ENDERITE
+		list.add(new ItemStack(ModBlocks.ENDERITE_ORE));
+		list.add(new ItemStack(ModItems.ENDERITE_SHARD));
+		list.add(new ItemStack(ModBlocks.ENDERITE_BLOCK));
+		list.add(new ItemStack(ModBlocks.ENDERITE_BRICKS));
+		
+		// OTHER
+		list.add(new ItemStack(ModBlocks.BOSS_ALTAR));
+		list.add(new ItemStack(ModBlocks.CUSTOM_GRASS));
+		list.add(new ItemStack(ModBlocks.CUSTOM_FLOWER));*/
 	}
 	
 	@Override
