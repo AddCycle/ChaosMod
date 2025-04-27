9515b05253e0beba487005ea1bcbaf36cf236b00
adel.belkercha <adel.belkercha@epita.fr>
2025-04-28 00:52:44 +0200

    fix: rendering the health bar of the boss perfectly

--- src/main/java/net/chaos/chaosmod/entity/boss/gui/BossBarRendering.java
+++ src/main/java/net/chaos/chaosmod/entity/boss/gui/BossBarRendering.java
@@ -13,6 +13,7 @@ import util.Reference;
 
 @EventBusSubscriber(Side.CLIENT)
 public class BossBarRendering {
+	public static final ResourceLocation PHASE_1 = new ResourceLocation(Reference.MODID, "textures/gui/entity/boss/revenge_blaze_bar.png");
 
 	@SubscribeEvent
 	public void onRenderBossBar(RenderGameOverlayEvent.BossInfo event) {
@@ -35,24 +36,29 @@ public class BossBarRendering {
 	    ScaledResolution scaledRes = new ScaledResolution(mc);
 	    int width = scaledRes.getScaledWidth();
 	    
-	    mc.getTextureManager().bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/entity/boss/revenge_blaze_bar.png"));
+	    mc.getTextureManager().bindTexture(PHASE_1);
 
+	    /*
+	     * Change this barWidth, fullHeight in order to match the texture
+	     */
 	    int barWidth = 182;
-	    int barHeight = 20;
-	    int x = (width / 2) - (barWidth / 2);
+	    int fullHeight = 40;
+	    int barHeight = 20; // Height of one section
+	    int x = (width - barWidth) / 2;
 	    int y = 20; // distance from top
 
-	    Gui gui = new Gui();
-		// Draw full bar background
-	    gui.drawTexturedModalRect(x, y, 0, 0, barWidth, barHeight);
+	    // Draw background (top 0-20 pixels of the texture)
+	    // gui.drawTexturedModalRect(x, y, 0, 0, barWidth, barHeight);
 
-	    // Draw health bar fill
+	    // Draw health bar fill (bottom 20-40 pixels of the texture)
 	    int healthWidth = (int)(bossInfo.getPercent() * (float)barWidth);
 	    if (healthWidth > 0) {
-	        gui.drawTexturedModalRect(x, y, 0, barHeight, healthWidth, barHeight);
+	    	Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 20, healthWidth, barHeight, barWidth, fullHeight);
 	    }
 
-	    // Draw boss name
+	    Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 0, barWidth, barHeight, barWidth, fullHeight);
+
+	    // Draw boss name above
 	    String bossName = bossInfo.getName().getFormattedText();
 	    mc.fontRenderer.drawStringWithShadow(bossName, (width / 2) - (mc.fontRenderer.getStringWidth(bossName) / 2), y - 10, 0xFFFFFF);
 	}
