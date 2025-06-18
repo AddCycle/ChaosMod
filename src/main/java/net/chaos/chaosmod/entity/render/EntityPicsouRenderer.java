87e272180c98b0813ab25bcde66b8796afc41180
adel.belkercha <adel.belkercha@epita.fr>
2025-06-18 20:41:53 +0200

    feat: now picsou playing particles but maybe resize them -> todo todo: resize particle + fix server crashing + fix picsou AI stopping

--- src/main/java/net/chaos/chaosmod/entity/render/EntityPicsouRenderer.java
+++ src/main/java/net/chaos/chaosmod/entity/render/EntityPicsouRenderer.java
@@ -1,15 +1,20 @@
 package net.chaos.chaosmod.entity.render;
 
+import net.chaos.chaosmod.client.particles.CoinParticles;
 import net.chaos.chaosmod.entity.EntityPicsou;
 import net.chaos.chaosmod.entity.model.ModelPixou;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import util.Reference;
 
 public class EntityPicsouRenderer extends RenderLiving<EntityPicsou> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(Reference.MODID, "textures/entity/picsou.png");
+	private int tickCounter = 0;
 
 	public EntityPicsouRenderer(RenderManager rendermanagerIn) {
 		super(rendermanagerIn, new ModelPixou(), 1.0f);
@@ -17,6 +22,30 @@ public class EntityPicsouRenderer extends RenderLiving<EntityPicsou> {
 		// this.addLayer(new LayerCapeViking(this));
 		// this.addLayer(new LayerCreeperCharge(new RenderCreeper(rendermanagerIn)));
 	}
+	
+	@Override
+	public void doRender(EntityPicsou entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		tickCounter++;
+		if (tickCounter >= 30) {
+			spawnCoinParticles(entity.getEntityWorld(), entity);
+			tickCounter = 0;
+		}
+		// TODO Auto-generated method stub
+		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+	}
+
+	public void spawnCoinParticles(World worldIn, EntityCreature entity)
+    {
+        double d0 = (double)(-MathHelper.sin(entity.rotationYaw * 0.017453292F));
+        double d1 = (double)MathHelper.cos(entity.rotationYaw * 0.017453292F);
+        
+        Minecraft mc = Minecraft.getMinecraft();
+        
+        if (worldIn.isRemote) mc.effectRenderer.addEffect(
+        	new CoinParticles(mc.getTextureManager(), worldIn, entity.posX, entity.posY + (double)entity.height * 0.5D, entity.posZ, d0, 0.0D, d1));
+
+        // ((WorldServer)worldIn).spawnParticle(EnumParticleTypes.SWEEP_ATTACK, playerIn.posX + d0, playerIn.posY + (double)playerIn.height * 0.5D, playerIn.posZ + d1, 0, d0, 0.0D, d1, 0.0D);
+    }
 
 	@Override
 	protected ResourceLocation getEntityTexture(EntityPicsou entity) {
